2c61defeea604a8ca537c1bf353f223bbc98025d
Trygve Wastvedt <twastvedt@azahner.com>
2018-06-25 22:10:55 -0500

    Fix behaviors registration

--- client/systems/TickOrder.system.ts
+++ client/systems/TickOrder.system.ts
@@ -11,12 +11,22 @@ export interface TickOrderSys extends AFrame.System {
 
 export const TickOrderSysDef: AFrame.SystemDefinition<TickOrderSys> = {
 	playComp: function(comp) {
-		this.el.behaviors.tick.forEach((c: OrderedTickComponent, i) => {
-			if (c.tickOrder > comp.tickOrder) {
-				this.el.behaviors.tick.splice(i, 0, comp);
-				return;
-			}
+		// Add behavior before first tick in list with a larger tickOrder. If none found, add to end.
+		const index = this.el.behaviors.tick.indexOf(comp);
+		if (index !== -1) { return; }
+
+		if (
+			this.el.behaviors.tick.every((c: OrderedTickComponent, i) => {
+				if (c.tickOrder > comp.tickOrder) {
+					this.el.behaviors.tick.splice(i, 0, comp);
+					return false;
+				}
+				return true;
+			}, this)
+		) {
+
 			this.el.behaviors.tick.push(comp);
-		});
+
+		}
 	}
 };
