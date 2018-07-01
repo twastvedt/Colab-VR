18c22f2676add1462022537343de1453a1ae4b75
Trygve Wastvedt <twastvedt@azahner.com>
2018-07-01 09:18:54 -0500

    tslint fixes.

--- client/app.ts
+++ client/app.ts
@@ -9,21 +9,22 @@ require('networked-aframe');
 require('mousetrap');
 
 
-import { LockedPointerComp } from "./components/LockedPointer.component";
-import { SlidingPointerComp } from "./components/SlidingPointer.component";
-import { SnapComp } from "./components/Snap.component";
-import { TestRotateComp } from "./components/TestRotate.component";
-import { CursorGeoComp } from "./components/CursorGeo.component";
-import { LockedTrackComp } from "./components/LockedTrack.component";
-import { ConstantScaleComp } from "./components/ConstantScale.component";
+import { LockedPointerComp } from './components/LockedPointer.component';
+import { SlidingPointerComp } from './components/SlidingPointer.component';
+import { SnapComp } from './components/Snap.component';
+import { TestRotateComp } from './components/TestRotate.component';
+import { CursorGeoComp } from './components/CursorGeo.component';
+import { LockedTrackComp } from './components/LockedTrack.component';
+import { ConstantScaleComp } from './components/ConstantScale.component';
+import { VelocityCompDef } from './components/Velocity.component';
 
-import { GridMatCompDef } from "./components/GridMaterial.component";
-import { OutlineMatCompDef } from "./components/OutlineMaterial.component";
-import { ApplyMatCompDef } from "./components/ApplyMaterial.component";
+import { GridMatCompDef } from './components/GridMaterial.component';
+import { OutlineMatCompDef } from './components/OutlineMaterial.component';
+import { ApplyMatCompDef } from './components/ApplyMaterial.component';
 
-import { GridMatSysDef } from "./systems/GridMaterial.system";
-import { DrawingSystem } from "./systems/Drawing.system";
-import { TickOrderSysDef } from "./systems/TickOrder.system";
+import { GridMatSysDef } from './systems/GridMaterial.system';
+import { DrawingSystem } from './systems/Drawing.system';
+import { TickOrderSysDef } from './systems/TickOrder.system';
 
 
 AFRAME.registerComponent('locked-pointer', LockedPointerComp);
@@ -32,6 +33,7 @@ AFRAME.registerComponent('snap', SnapComp);
 AFRAME.registerComponent('cursor-geo', CursorGeoComp);
 AFRAME.registerComponent('locked-track', LockedTrackComp);
 AFRAME.registerComponent('constant-scale', ConstantScaleComp);
+AFRAME.registerComponent('velocity', VelocityCompDef);
 
 AFRAME.registerComponent('grid-mat', GridMatCompDef);
 AFRAME.registerComponent('outline', OutlineMatCompDef);
