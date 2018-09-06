7becaa20b988d435716977070f6ad6f5a588e73d
Trygve Wastvedt <trygve.wastvedt@gmail.com>
2018-09-05 21:58:08 -0500

    Get test-2 back.

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const http = require("http"),
-	express = require("express"),
-	socketIo = require("socket.io"),
-	easyrtc = require("easyrtc"),
+const http = require('http'),
+	express = require('express'),
+	socketIo = require('socket.io'),
+	easyrtc = require('easyrtc'),
 	livereload = require('livereload'),
 	nunjucks = require('nunjucks');
 
 // Set process name
-process.title = "node-easyrtc";
+process.title = 'node-easyrtc';
 
 // Get port or default to 8080
 const port = process.env.PORT || 8080;
@@ -15,20 +15,25 @@ const port = process.env.PORT || 8080;
 const server = livereload.createServer({
 	delay: 200
 });
-server.watch(__dirname + "/dist");
-server.watch(__dirname + "/client/views");
+server.watch(__dirname + '/dist');
+server.watch(__dirname + '/client/views');
 
 // Setup and configure Express http server.
 const app = express();
 
 nunjucks.configure('client/views', {
-    autoescape: true,
+	autoescape: true,
 	express: app,
 	watch: true
 });
 
 app.get('/', function(req, res) {
-    res.render('index.html');
+	res.render('index.html');
+});
+
+
+app.get('/test-2', function(req, res) {
+	res.render('test-2.html');
 });
 
 app.use(express.static('dist'));
@@ -39,13 +44,13 @@ var webServer = http.createServer(app);
 
 // Start Socket.io so it attaches itself to Express server
 // @ts-ignore
-var socketServer = socketIo.listen(webServer, {"log level":1});
+var socketServer = socketIo.listen(webServer, { 'log level': 1 });
 
 var myIceServers = [
-	{"url":"stun:stun.l.google.com:19302"},
-	{"url":"stun:stun1.l.google.com:19302"},
-	{"url":"stun:stun2.l.google.com:19302"},
-	{"url":"stun:stun3.l.google.com:19302"}
+	{'url': 'stun:stun.l.google.com:19302'},
+	{'url': 'stun:stun1.l.google.com:19302'},
+	{'url': 'stun:stun2.l.google.com:19302'},
+	{'url': 'stun:stun3.l.google.com:19302'}
 	// {
 	//   "url":"turn:[ADDRESS]:[PORT]",
 	//   "username":"[USERNAME]",
@@ -57,44 +62,44 @@ var myIceServers = [
 	//   "credential":"[CREDENTIAL]"
 	// }
 ];
-easyrtc.setOption("appIceServers", myIceServers);
-easyrtc.setOption("logLevel", "debug");
-easyrtc.setOption("demosEnable", false);
+easyrtc.setOption('appIceServers', myIceServers);
+easyrtc.setOption('logLevel', 'debug');
+easyrtc.setOption('demosEnable', false);
 
 // Overriding the default easyrtcAuth listener, only so we can directly access its callback
-easyrtc.events.on("easyrtcAuth", function(socket, easyRTCid, msg, socketCallback, callback) {
-	easyrtc.events.defaultListeners.easyrtcAuth(socket, easyRTCid, msg, socketCallback, function(err, connectionObj){
+easyrtc.events.on('easyrtcAuth', function(socket, easyRTCid, msg, socketCallback, callback) {
+	easyrtc.events.defaultListeners.easyrtcAuth(socket, easyRTCid, msg, socketCallback, function(err, connectionObj) {
 		if (err || !msg.msgData || !msg.msgData.credential || !connectionObj) {
 			callback(err, connectionObj);
 			return;
 		}
 
-		connectionObj.setField("credential", msg.msgData.credential, {"isShared":false});
+		connectionObj.setField('credential', msg.msgData.credential, {'isShared': false});
 
-		console.log("["+easyRTCid+"] Credential saved!", connectionObj.getFieldValueSync("credential"));
+		console.log('[' + easyRTCid + '] Credential saved!', connectionObj.getFieldValueSync('credential'));
 
 		callback(err, connectionObj);
 	});
 });
 
 // To test, lets print the credential to the console for every room join!
-easyrtc.events.on("roomJoin", function(connectionObj, roomName, roomParameter, callback) {
-	console.log("["+connectionObj.getEasyrtcid()+"] Credential retrieved!", connectionObj.getFieldValueSync("credential"));
+easyrtc.events.on('roomJoin', function(connectionObj, roomName, roomParameter, callback) {
+	console.log('[' + connectionObj.getEasyrtcid() + '] Credential retrieved!', connectionObj.getFieldValueSync('credential'));
 	easyrtc.events.defaultListeners.roomJoin(connectionObj, roomName, roomParameter, callback);
 });
 
 // Start EasyRTC server
 var rtc = easyrtc.listen(app, socketServer, null, function(err, rtcRef) {
-	console.log("Initiated");
+	console.log('Initiated');
 
-	rtcRef.events.on("roomCreate", function(appObj, creatorConnectionObj, roomName, roomOptions, callback) {
-		console.log("roomCreate fired! Trying to create: " + roomName);
+	rtcRef.events.on('roomCreate', function(appObj, creatorConnectionObj, roomName, roomOptions, callback) {
+		console.log('roomCreate fired! Trying to create: ' + roomName);
 
 		appObj.events.defaultListeners.roomCreate(appObj, creatorConnectionObj, roomName, roomOptions, callback);
 	});
 });
 
-//listen on port
+// listen on port
 webServer.listen(port, function () {
 	console.log('listening on http://localhost:' + port);
 });
